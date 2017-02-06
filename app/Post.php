9e7d72968e58f8250b2234b00dbde7716e7be501
Apecengo <apecengo@gmail.com>
2017-02-06 16:29:26 +0100

    Añadir Post al panel de admin

--- app/Post.php
+++ app/Post.php
@@ -5,9 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+use Carbon\Carbon;
+
 class Post extends Model
 {
     use SoftDeletes;
+    use \Backpack\CRUD\CrudTrait, \Venturecraft\Revisionable\RevisionableTrait;
 
     /**
      * The attributes that should be mutated to dates.
@@ -15,4 +18,33 @@ class Post extends Model
      * @var array
      */
     protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'content', 'announcement', 'user_id',
+    ];
+
+    public function user() {
+    	return $this->belongsTo('App\User');
+    }
+
+    public function isNew() {
+    	$dt = $this->created_at;
+    	$dt->addDays(2);
+    	return $dt->isFuture();
+    }
+
+    public function getCreatedDiff() {
+        $last = $this->created_at;
+
+        $dt = Carbon::parse($last);
+
+        Carbon::setLocale('es');
+        return $dt->diffForHumans();
+    }
+
 }
--- resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -23,8 +23,9 @@
 
           <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/user') }}"><i class="fa fa-user"></i> <span>Usuarios</span></a></li>
           <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/specialty') }}"><i class="fa fa-book"></i> <span>Especialidades</span></a></li>
-          <li><a href="{{ url(config('backpack.base.route_prefix').'/page') }}"><i class="fa fa-file-o"></i> <span>Pages</span></a></li>
-          <li><a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/setting') }}"><i class="fa fa-cog"></i> <span>Settings</span></a></li>
+          <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/post') }}"><i class="fa fa-user"></i> <span>Comunicados</span></a></li>
+          <li><a href="{{ url(config('backpack.base.route_prefix').'/page') }}"><i class="fa fa-file-o"></i> <span>Páginas</span></a></li>
+          <li><a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/setting') }}"><i class="fa fa-cog"></i> <span>Ajustes</span></a></li>
 
           <!-- ======================================= -->
           <li class="header">{{ trans('backpack::base.user') }}</li>
