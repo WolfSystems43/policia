8c8d081205a515ca133c2d8ce5c1510aadb7734f
Apecengo <apecengo@gmail.com>
2017-02-27 23:29:05 +0100

    Ajustes en lista de badges

--- resources/views/badges/list.blade.php
+++ resources/views/badges/list.blade.php
@@ -13,30 +13,17 @@
 	@foreach($badges as $badge)
 		<div class="row">
 			<div class="col s4 m3 l2">
+				@if(!is_null($badge->image))
 				<center><img height="50" src="{{ url($badge->image) }}" alt="{{ $badge->name }}"></center>
+				@endif
 			</div>
 			<div class="col s8 m9 l10 black-text">
-			<b>{{ $badge->name }}</b>
+			<b><a href="{{ route('badge', ['id' => $badge->id]) }}">{{ $badge->name }}</a></b>
 			<br><span>0 concedidas</span>
 			</div>
 		</div>
 	@endforeach
 		</div>
-
-	<p>Diplomas</p>
-	<div class="card-panel">
-		<br>
-		@foreach($certificates as $certificate)
-			<div class="row">
-				<div class="col s4 m3 l2">
-				</div>
-				<div class="col s8 m9 l10 black-text">
-				<b>{{ $certificate->name }}</b>
-				<br><span>0 concedidos</span>
-				</div>
-			</div>
-		@endforeach	
-	</div>
 
 	<p>Licencias</p>
 	<div class="card-panel">
@@ -44,14 +31,37 @@
 		@foreach($licenses as $licence)
 			<div class="row">
 				<div class="col s4 m3 l2">
+					@if(!is_null($licence->image))
+					<center><img height="50" src="{{ url($licence->image) }}" alt="{{ $licence->name }}"></center>
+					@endif
 				</div>
 				<div class="col s8 m9 l10 black-text">
-				<b>{{ $licence->name }}</b>
+				<b><a href="{{ route('badge', ['id' => $licence->id]) }}">{{ $licence->name }}</a></b>
 				<br><span>0 concedidos</span>
 				</div>
 			</div>
 		@endforeach	
 	</div>
+
+	<p>Diplomas</p>
+	<div class="card-panel">
+		<br>
+		@foreach($certificates as $certificate)
+			<div class="row">
+				<div class="col s4 m3 l2">
+					@if(!is_null($certificate->image))
+					<center><img height="50" src="{{ url($certificate->image) }}" alt="{{ $certificate->name }}"></center>
+					@endif
+				</div>
+				<div class="col s8 m9 l10 black-text">
+				<b><a href="{{ route('badge', ['id' => $certificate->id]) }}">{{ $certificate->name }}</a></b>
+				<br><span>0 concedidos</span>
+				</div>
+			</div>
+		@endforeach	
+	</div>
+
+
 
 	<p></p>
 </div>
