4f819c86ce06460b96d971b494b315dcad587fa1
Apecengo <apecengo@gmail.com>
2017-03-07 21:39:35 +0100

    Ajustes de correo vista

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -17,14 +17,35 @@ $unlock_page = true;
 		<div class="card-panel">
 		  <form action="#" method="post">
 		  {{ csrf_field() }}
-		  <p>Ajustes visuales</p>
-		    <p>
-		      <input type="checkbox" name="dark_mode" class="filled-in" id="filled-in-box" @if(session('dark', false)) checked="checked" @endif />
-		      <label for="filled-in-box">Modo oscuro</label>
-		    </p>
 			
+			<div class="row">
+				<div class="col s12">
+				    <div class="input-field">
+				      <input type="checkbox" name="dark_mode" class="filled-in" id="filled-in-box" @if(session('dark', false)) checked="checked" @endif />
+				      <label for="filled-in-box">Modo oscuro</label>
+				    </div>	
+				</div>
+				<br><br><br><br>
+				<div class="col s12">
+					<div class="input-field">
+				    	<input id="email_input" type="email" value="{{ Auth::user()->email }}" readonly="readonly">
+				    	<label for="email_input">Correo electrónico</label>
+					</div>
+				</div>
+				<div class="col s12">
+				  <!-- Disabled Switch -->
+				  <div class="switch">
+				    <label>
+				      Solo notificaciones críticas
+				      <input type="checkbox" name="email_enabled" @if(Auth::user()->emailEnabled()) checked="checked" @endif>
+				      <span class="lever"></span>
+				      Todas las notificaciones
+				    </label>
+				  </div>
+				</div>
+			</div>
 			<br>
-		    <button type="submit" class="btn">Guardar ajustes</button>
+		    <button type="submit" class="btn green waves-effect">Guardar</button>
 		  </form>
 		</div>
 	</div>
